da4ff3b6ab3fa08769451d941eb25eee52b83cb6
claudiosmweb <contato@claudiosmweb.com>
2013-05-25 02:44:16 -0300

    improved the validate_options method

--- powercomment.php
+++ powercomment.php
@@ -368,7 +368,7 @@ class Power_Comment {
             if ( isset( $input[$key] ) ) {
 
                 // Strip all HTML and PHP tags and properly handle quoted strings.
-                $output[$key] = strip_tags( stripslashes( $input[$key] ) );
+                $output[$key] = sanitize_text_field( $input[$key] );
             }
         }
 
